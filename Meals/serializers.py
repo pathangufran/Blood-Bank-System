9ae58dd6973aa249c3b4cde09a9e7821000417ec
Gufran <81221327+pathangufran@users.noreply.github.com>
2023-07-04 23:35:04 +0530

    Delete Meals directory

--- Meals/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from Meals.models import Food
-
-class FoodSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Food
-        fields = '__all__'
--- Meals/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from Meals import views
-urlpatterns = [
-
-    path('search',views.Search,name='search'),
-    path('search_view',views.search_view,name='search_view'),
-
-]
